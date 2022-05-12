6c7c7f7d704d039bee13160216345b569577b54a
Andrey Shaporenko <shaporenkoae@gmail.com>
2022-05-12 21:58:47 +0300

    Task 9-16

---
+++ Task16/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите два числа: ");
+
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA == numberB * numberB)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+
---
+++ Task9/Program.cs
@@ -0,0 +1,13 @@
+﻿int randomNumber = new Random().Next(10, 100);
+
+int numberOne = randomNumber % 10;
+int numberTwo = randomNumber / 10;
+Console.Write($"У Числа {randomNumber} ,большая цифра ");
+if (numberOne > numberTwo)
+{
+    Console.Write(numberOne);
+}
+else
+{
+    Console.Write(numberTwo);
+}
