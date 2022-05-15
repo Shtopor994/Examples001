ae3b249a5f59614e5d30022cb499ff28c8b4e759
Andrey Shaporenko <shaporenkoae@gmail.com>
2022-05-15 21:55:52 +0300

    Task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.WriteLine("Введите трехзначное число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA > 99 & numberA < 1000)
+{
+    int numberOne = numberA / 100;
+    int numberTwo = (numberA - numberOne * 100) / 10;
+    int numberThree = numberA - numberOne * 100 - numberTwo * 10;
+    Console.Write(numberTwo);
+}
+else
+{
+    Console.Write("Сэр, проверьте введенное число");
+}
+
