1d96c227a8f114f58280600f6723fcc8a0b535d9
Andrey Shaporenko <shaporenkoae@gmail.com>
2022-05-23 21:59:42 +0300

    task 31-35

---
+++ Task31/Program.cs
@@ -0,0 +1,47 @@
+﻿/* See https://aka.ms/new-console-template for more information
+Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
+*/
+
+int[] getRandomArray(int length, int deviation)
+{
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(-deviation, deviation + 1);
+    }
+    return result;
+}
+
+void printArray(int[] arrayToPrint)
+{
+    Console.WriteLine("[");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        Console.WriteLine(arrayToPrint[i]);
+        if (i != arrayToPrint.Length)
+        {
+            Console.WriteLine(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+int sumElementsOfArray(int[] arrayToCalculate, int multiplay)
+{
+    int result = 0;
+    for (int i = 0; i < arrayToCalculate.Length; i++)
+    {
+        if(arrayToCalculate[i] * multiplay > 0)
+        {
+        result += arrayToCalculate[i];
+        }
+    }
+    return result;
+}
+
+
+int[] randomArray = getRandomArray(12,1);
+int positiveSumOfArray = sumElementsOfArray(randomArray, 1);
+int negativeSumOfArray = sumElementsOfArray(randomArray, -1);
+printArray(randomArray);
+Console.WriteLine($"сумма положительных элементов {positiveSumOfArray}, сумма отрицательных {negativeSumOfArray}");
---
+++ Task35/Program.cs
@@ -0,0 +1,35 @@
+﻿/* 
+Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+[5, 18, 123, 6, 2] -> 1
+[1, 2, 3, 6, 2] -> 0
+[10, 11, 12, 13, 14] -> 5
+
+*/
+
+void FillArray(int[] collection)
+{
+    for (int index = 0; index < collection.Length; index++)
+    {
+        collection[index] = new Random().Next(-200, 200);
+    }
+}
+int[] array1 = new int[123];
+FillArray(array1);
+
+int findDiapazon(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if ( array[i] > 10 && array[i] < 100)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int count = findDiapazon(array1);
+Console.WriteLine($"{count}"); 
+
+
