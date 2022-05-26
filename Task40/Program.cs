724126a3e56bd0112ff1f14fa02c69e0ca842c1f
Andrey Shaporenko <shaporenkoae@gmail.com>
2022-05-26 21:57:30 +0300

    task 39-44

---
+++ Task40/Program.cs
@@ -0,0 +1,31 @@
+﻿/* 
+Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+*/
+
+int[] arraySides()
+{
+    int[] result = new int[3];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = Convert.ToInt32(Console.ReadLine());
+
+    }
+    return result;
+}
+
+bool compareSide(int[] arrayToCompare)
+{
+    bool result = false;
+    if (   arrayToCompare[0] + arrayToCompare[1] > arrayToCompare[2]
+        && arrayToCompare[1] + arrayToCompare[2] > arrayToCompare[0]
+        && arrayToCompare[0] + arrayToCompare[2] > arrayToCompare[1])
+    {
+        result = true;
+    }
+    return result;
+}
+
+int[] array = arraySides();
+bool result = compareSide(array);
+Console.WriteLine(result);
