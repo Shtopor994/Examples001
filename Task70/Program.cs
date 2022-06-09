0375118a7afaa91a30ce9463dffcb1a5a67ecafe
Andrey Shaporenko <shaporenkoae@gmail.com>
2022-06-09 22:06:15 +0300

    T 70-72

---
+++ Task70/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 70: Напишите программу, которая на вход принимает два числа и выдаёт первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+3 и 4, N = 5 -> 3 4 7 11 18
+6 и 10, N = 4 -> 6 10 16 26 
+*/
+
+
+
+string UpToNumberPlus(int a, int b, int N)
+{
+    if (N == 0)
+    {
+        return " ";
+    }
+     else
+     {
+         return $"{a} " + UpToNumberPlus(b, a + b, N - 1);
+     }
+}
+string result = UpToNumberPlus(3, 4, 5);
+Console.WriteLine(result);
---
+++ Task71/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 71: В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в». Покажите все слова, состоящие из n букв, которые можно построить из букв этого алфавита.
+n = 2 -> аа, ии, сс, вв, аи, иа, ис, си, ас, са, 
+ав, ва, ви, ив, св, вс
+*/
+
+void UniqWords(string alfabet, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{new String(word)}");
+    }
+    else
+    {
+    for (int i = 0; i < alfabet.Length; i++)
+    {
+        word[length] = alfabet[i];
+        UniqWords(alfabet, word, length + 1);
+    }
+    }
+}
+UniqWords("аисв", new char[2]);
+
