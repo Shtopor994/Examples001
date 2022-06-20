12cedf5cb22a186638b4c19e1155de79924fe4c0
Andrey Shaporenko <shaporenkoae@gmail.com>
2022-06-20 17:41:26 +0300

    T 54.1

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -13,10 +13,33 @@ int InputNumbers(string input)
   return output;
 }
 
-
 int[,] array = new int[m, n];
 CreateArray(array);
 FillArray(array);
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void FillArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
 
 Console.WriteLine("Отсортированный массив: ");
 SortArrayLines(array);
@@ -42,25 +65,3 @@ void SortArrayLines(int[,] array)
 }
 
 
-void CreateArray(int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      array[i, j] = new Random().Next(range);
-    }
-  }
-}
-
-void FillArray(int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      Console.Write(array[i, j] + " ");
-    }
-    Console.WriteLine();
-  }
-}
